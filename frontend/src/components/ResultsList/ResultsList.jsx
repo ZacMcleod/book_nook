fb1f5be1596fa820bdbffbe56dafff342161d698
Burkard920 Burkard <stevenburkard24@yahoo.com>
2023-08-02 16:59:56 -0500

    Added ResultsList.css to ResultsList component to style the data being displayed

--- frontend/src/components/ResultsList/ResultsList.jsx
+++ frontend/src/components/ResultsList/ResultsList.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './ResultsList.css';
 
 
 const ResultsList = ({ searchResults }) => {
     return (
         <div>
-            <h3>Book Search Results:
+            <h2>Book Search Results:
 
-            </h3>
+            </h2>
             {searchResults.map((items, index) => (
-                <Link to={`/details/${items.id}`} key={index}> 
-                    <div>
-                        <h4>{items.volumeInfo.title}</h4>
-                    </div>
+                <Link to={`/details/${items.id}`} key={index}>
+                        <div className="border-element">
+                            <h4>{items.volumeInfo.title}</h4>
+                        </div>
                 </Link>
             ))}
         </div>
